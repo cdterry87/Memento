96eddb422c25a6ea3672999b282eb570478bae78
= <chase.terry87@gmail.com>
2019-10-13 10:00:34 -0500

    Getting started with Laravel and Vuetify

---
+++ resources/js/components/App.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <v-app class="inspire">
+            <v-toolbar color="transparent" flat dense>
+                <v-toolbar-title>
+                    <a href="/home" class="headline logo">{{ title }}</a>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                    <v-btn text to="/home" v-if="this.$router.currentRoute.name != 'home'">
+                        <v-icon>mdi-home</v-icon>
+                        <span class="ml-3" v-if="$vuetify.breakpoint.mdAndUp">Home</span>
+                    </v-btn>
+                    <v-btn text to="/account">
+                        <v-icon>mdi-account-circle</v-icon>
+                        <span class="ml-3" v-if="$vuetify.breakpoint.mdAndUp">My Account</span>
+                    </v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+
+            <v-content>
+                <router-view></router-view>
+
+                <v-snackbar v-model="snackbar.enabled" :color="snackbar.color" :bottom="true" :timeout="snackbar.timeout">
+                    {{ snackbar.message }}
+                    <v-btn color="white" text @click="snackbar.enabled = false"><v-icon>mdi-close</v-icon></v-btn>
+                </v-snackbar>
+            </v-content>
+
+            <div class="footer caption text-center mt-5 mb-3">
+                {{ title }} &copy; 2019
+            </div>
+        </v-app>
+    </div>
+</template>
+
+<script>
+import Event from './../events'
+
+export default {
+    name: 'App',
+    props: ['title'],
+    data() {
+        return {
+            snackbar: {
+                enabled: false,
+                message: '',
+                timeout: 5000,
+                color: ''
+            },
+        }
+    },
+    created() {
+        Event.$on('success', message => {
+            this.snackbar.message = message
+            this.snackbar.color = 'success'
+            this.snackbar.enabled = true
+        })
+        Event.$on('warning', message => {
+            this.snackbar.message = message
+            this.snackbar.color = 'warning'
+            this.snackbar.enabled = true
+        })
+        Event.$on('error', message => {
+            this.snackbar.message = message
+            this.snackbar.color = 'error'
+            this.snackbar.enabled = true
+        })
+    }
+}
+</script>
